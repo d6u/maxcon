63ebfb2c560ac91160972437e67f7af39803362d
Daiwei <daiweilu123@gmail.com>
2016-04-04 10:28:42 -0400

    Improve module entry exporting format

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import Maxcon from './Maxcon';
-import {MaxconTaskConfig} from './utils';
-
-export {
-  Maxcon as default,
-  MaxconTaskConfig,
-};
-
-// Will overwrite ES2015 export above
-// to work around common js format in Node
-module.exports = Maxcon;
+export {default as Maxcon} from './Maxcon';
+export {MaxconTaskConfig, TaskConfig} from './utils';
